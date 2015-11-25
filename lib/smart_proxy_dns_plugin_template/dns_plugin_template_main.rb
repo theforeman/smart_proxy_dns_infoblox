c93acea5c96a78044ccc10967b59963b089588af
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2015-11-25 14:34:39 +0000

    Updated to reflect latest changes in dns module

--- lib/smart_proxy_dns_plugin_template/dns_plugin_template_main.rb
+++ lib/smart_proxy_dns_plugin_template/dns_plugin_template_main.rb
@@ -1,58 +1,46 @@
-require 'dns/dns'
+require 'dns_common/dns_common'
 
 module Proxy::Dns::PluginTemplate
   class Record < ::Proxy::Dns::Record
     include Proxy::Log
     include Proxy::Util
 
-    attr_reader :example_setting
+    attr_reader :example_setting, :optional_path, :required_setting, :required_path
 
-    def self.record(attrs = {})
-      new(attrs.merge(:example_setting => ::Proxy::Dns::PluginTemplate::Plugin.settings.example_setting))
+    def initialize
+      @required_setting = ::Proxy::Dns::PluginTemplate::Plugin.settings.required_setting # never nil
+      @example_setting = ::Proxy::Dns::PluginTemplate::Plugin.settings.example_setting # can be nil
+      @required_path = ::Proxy::Dns::PluginTemplate::Plugin.settings.required_path # file exists and is readable
+      @optional_path = ::Proxy::Dns::PluginTemplate::Plugin.settings.optional_path # nil, or file exists and is readable
+
+      # Common settings can be defined by the main plugin, it's ok to use them locally.
+      # Please note that providers must not rely on settings defined by other providers or plugins they are not related to.
+      super('localhost', ::Proxy::Dns::Plugin.settings.dns_ttl)
     end
 
-    def initialize options = {}
-      @example_setting = options[:example_setting]
-      raise "dns_plugin_template provider needs 'example_setting' option" unless example_setting
-      super(options)
+    # Calls to these methods are guaranteed to have non-nil parameters
+    def create_a_record(fqdn, ip)
+      # FIXME: add a forward 'A' record with fqdn, ip
+      # Raise an error if the FQDN is already in DNS but with a different IP:
+      #   raise(Proxy::Dns::Collision, "#{fqdn} is already used by #{ip_in_use}")
     end
 
-    def create
-      case @type
-        when "A"
-          # FIXME: add a forward 'A' record with @fqdn, @ttl, @type, @value (IP)
-          #
-          # Raise an error if the FQDN is already in DNS but with a different IP:
-          #   raise(Proxy::Dns::Collision, "#{@fqdn} is already used by #{ip}")
-
-          true
-        when "PTR"
-          # FIXME: add a reverse 'PTR' record with @value (IP), @fqdn, @ttl, @type
-          #
-          # Raise an error if the IP is already in DNS but with a different FQDN:
-          #   raise(Proxy::Dns::Collision, "#{@value} is already used by #{name}")
-
-          true
-      end
+    def create_ptr_record(fqdn, ip)
+      # FIXME: add a reverse 'PTR' record with ip, fqdn
+      # Raise an error if the IP is already in DNS but with a different FQDN:
+      #   raise(Proxy::Dns::Collision, "#{ip} is already used by #{fqdn_in_use}")
     end
 
-    def remove
-      case @type
-        when "A"
-          # FIXME: remove the forward 'A' record with @fqdn, @ttl, @type, @value (IP)
-          #
-          # Raise an error if the FQDN is not in DNS:
-          #   raise Proxy::Dns::NotFound.new("Cannot find DNS entry for #{@fqdn}")
+    def remove_a_record(fqdn)
+      # FIXME: remove the forward 'A' record with fqdn
+      # Raise an error if the FQDN is not in DNS:
+      #   raise Proxy::Dns::NotFound.new("Cannot find DNS entry for #{fqdn}")
+    end
 
-          true
-        when "PTR"
-          # FIXME: remove the reverse 'PTR' record with @value (IP), @fqdn, @ttl, @type
-          #
-          # Raise an error if the IP is not in DNS:
-          #   raise Proxy::Dns::NotFound.new("Cannot find DNS entry for #{@value}")
-
-          true
-      end
+    def remove_ptr_record(ip)
+      # FIXME: remove the reverse 'PTR' record with ip
+      # Raise an error if the IP is not in DNS:
+      #   raise Proxy::Dns::NotFound.new("Cannot find DNS entry for #{ip}")
     end
   end
 end
--- lib/smart_proxy_dns_plugin_template/dns_plugin_template_plugin.rb
+++ lib/smart_proxy_dns_plugin_template/dns_plugin_template_plugin.rb
@@ -2,13 +2,22 @@ require 'smart_proxy_dns_plugin_template/dns_plugin_template_version'
 
 module Proxy::Dns::PluginTemplate
   class Plugin < ::Proxy::Provider
-    plugin :dns_plugin_template, ::Proxy::Dns::PluginTemplate::VERSION,
-           :factory => proc { |attrs| ::Proxy::Dns::PluginTemplate::Record.record(attrs) }
+    plugin :dns_plugin_template, ::Proxy::Dns::PluginTemplate::VERSION
 
-    requires :dns, '>= 1.10'
+    # Settings listed under default_settings are required.
+    # An exception will be raised if they are initialized with nil values.
+    # Settings not listed under default_settings are considered optional and by default have nil value.
+    default_settings :required_setting => 'default_value', :required_path => '/must/exist'
+
+    requires :dns, '>= 1.11'
+
+    # Verifies that a file exists and is readable.
+    # Uninitialized optional settings will not trigger validation errors.
+    validate_readable :required_path, :optional_path
 
     after_activation do
       require 'smart_proxy_dns_plugin_template/dns_plugin_template_main'
+      require 'smart_proxy_dns_plugin_template/dns_plugin_template_dependencies'
     end
   end
 end
