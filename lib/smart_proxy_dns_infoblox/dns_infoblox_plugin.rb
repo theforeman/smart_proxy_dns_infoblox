1e777c9d16f284c04890f5c15d8a3ded4fa7fffc
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-09-09 14:29:28 +0200

    Fix Layout/LineLength violation

--- lib/smart_proxy_dns_infoblox/dns_infoblox_plugin.rb
+++ lib/smart_proxy_dns_infoblox/dns_infoblox_plugin.rb
@@ -2,7 +2,8 @@ module Proxy::Dns::Infoblox
   class Plugin < ::Proxy::Provider
     plugin :dns_infoblox, ::Proxy::Dns::Infoblox::VERSION
 
-    default_settings :username => 'infoblox', :password => 'infoblox', :dns_server => 'localhost', :dns_view => 'default', :timeout => 60
+    default_settings :username => 'infoblox', :password => 'infoblox', :dns_server => 'localhost',
+                     :dns_view => 'default', :timeout => 60
 
     requires :dns, '>= 1.12'
 
